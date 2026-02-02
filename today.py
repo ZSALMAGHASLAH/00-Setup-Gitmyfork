15ee7417e47cc64b871f8c294396f7f9a0c73c41
Zainab <zainab20041212@gmail.com>
2026-02-02 19:04:09 +0300

    Complete both functions

--- today.py
+++ today.py
@@ -3,11 +3,9 @@
 
 # $CHALLENGIFY_BEGIN
 def my_name_is():
-    "my name is"
-    return "ZSALMAGHASLAH"
+    return "Zainab Saeed Almaghaslah"
 
 def my_age_is():
-    "my age is"
     return 21  
 
 # $CHALLENGIFY_END
